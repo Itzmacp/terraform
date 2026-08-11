549f2c1bd416819533ab38bee563e0830b0367e8
itzmacp <itzmacp@gmail.com>
2026-08-11 22:37:42 +0530

    up1

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "exmys3" {
+  bucket = "my-tf-test-bucketmaccloudtf001xx"
+
+  tags = {
+    Name        = "My Bucket - exmys3"
+    Environment = "Dev"
+  }
+}
